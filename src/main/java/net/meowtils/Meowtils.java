b9a9f3a8e854a3d40e41ee24687d6346142e4f91
meowmeow111112 <ermnope913@gmail.com>
2026-04-05 10:14:07 -0400

    Fix bug: checkpoint message not showing

--- src/main/java/net/meowtils/Meowtils.java
+++ src/main/java/net/meowtils/Meowtils.java
@@ -65,6 +65,6 @@ public class Meowtils {
         if (mc.thePlayer == null || mc.theWorld == null) return;
 
         teleportManager.onClientTick();
-        checkpointManager.onClientTick();
+        checkpointManager.onClientTick(configManager.getColor1(), configManager.getColor2(), configManager.getReset(), configManager.getPrefix());
     }
 }
